e4d6d4fc81e279a65de9e4888f0a144066edf3e4
Sergio Mercado <zerghiomario@gmail.com>
2017-12-17 22:37:15 -0800

    Built for prod

--- dist_assets/js/calorieCalculator.js
+++ dist_assets/js/calorieCalculator.js
@@ -1,14 +1,1 @@
-'use strict';
-
-function getBMR(age, sex, height, weight) {
-  var weightInKilos = weight / 2.20462,
-      bmr = 10 * weightInKilos + 6.25 * height - 5 * age;
-
-  if (sex === 'female') {
-    bmr -= 161;
-  } else {
-    bmr += 5;
-  }
-
-  return bmr;
-}
+'use strict';function getBMR(a,b,c,d){var e=10*(d/2.20462)+6.25*c-5*a;return'female'===b?e-=161:e+=5,e}
--- src/js/main.js
+++ src/js/main.js
@@ -16,15 +16,12 @@ if ('serviceWorker' in navigator) {
 
   window.addEventListener('load', function(e) {
 
-    window.applicationCache.addEventListener('updateready', function(e) {
+    window.applicationCache.addEventListener('updateready', e => {
       if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
         // Browser downloaded a new app cache.
         if (confirm('A new version of this site is available. Load it?')) {
           window.location.reload();
         }
-      } else {
-        alert('Manifest didn\'t changed. Nothing new to server.');
-        // Manifest didn't changed. Nothing new to server.
       }
     }, false);
 
